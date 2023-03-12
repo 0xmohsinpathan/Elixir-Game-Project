82c0cbd0d70cc1dc3d0e73601ea5b55714babad2
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-12 12:32:35 +0530

    💥 NEW: add new function for wordle.

--- lib/games/wordle.ex
+++ lib/games/wordle.ex
@@ -1,5 +1,4 @@
 defmodule Games.Wordle do
-
   @moduledoc """
   Documentation for Games.Wordle
 
@@ -10,6 +9,7 @@ defmodule Games.Wordle do
   * yellow: letter is in the answer but in the wrong position
   * grey: letter is not in the answer
   """
+
   # TODO complete my wordle project
 
   # @doc """
@@ -21,14 +21,56 @@ defmodule Games.Wordle do
   # ```
   # """
 
-  # Client
+  # def play(guess_word) do
+  # end
 
+  def feedback(guess_word, answer_word) do
+    zip_list = zip_list(guess_word, answer_word) |> Enum.unzip()
 
+    zip_list
+    |> check_green()
+    |> check_yellow()
+    |> check_grey()
+  end
 
+  defp check_green({guess_list, answer_list}) do
+  Enum.zip(guess_list, answer_list)
+    |> Enum.map(fn
+      {char, char} -> {:green, nil}
+      pair -> pair
+    end)
+    |> Enum.unzip()
+    |> IO.inspect()
+  end
 
-  
+  defp check_yellow({guess_list, answer_list}) do
+    Enum.reduce(guess_list, {[], answer_list}, fn guess_char, {result, answer_list} ->
+      answer_index = Enum.find_index(answer_list, fn answer_char -> answer_char == guess_char end)
 
-  defp answer_guesses() do
+      if answer_index do
+        {result ++ [:yellow], List.replace_at(answer_list, answer_index, nil)} |> IO.inspect()
+      else
+        {result ++ [guess_char], answer_list} |> IO.inspect()
+      end
+    end)
+  end
+
+  defp check_grey({guess_list, answer_list}) do
+    Enum.zip(guess_list, answer_list)
+    |> Enum.map(fn
+      {:green, _answer_char} -> {:green, nil}
+      {:yellow, _answer_char} -> {:yellow, nil}
+      _ -> {:grey, nil}
+    end)|> Enum.unzip()
+  end
+
+  defp zip_list(guess_word, answer_word) do
+    list_answer_char = String.graphemes(answer_word)
+    list_guess_char = String.graphemes(guess_word)
+    Enum.zip(list_answer_char, list_guess_char)
+  end
+
+  defp answer_guesses do
     [
       "Apple",
       "Tiger",
