6a6b0e357195b43d3893af4f65a49164c6f73e50
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-13 17:16:38 +0530

    🐛 FIX:  in RPS games

--- lib/games/RockPaperScissors.ex
+++ lib/games/RockPaperScissors.ex
@@ -13,20 +13,27 @@ defmodule Games.RockPaperScissors do
   ```
   """
   def play do
-    IO.puts("Choose Rock Paper or Scissors!")
-    ai_choice = Enum.random([:rock, :paper, :scissors])
+    IO.puts("Choose Rock, Paper, Scissors!")
+    ai_choice = Enum.random([:rock]) #:paper, :scissors
     player_choice = IO.gets(["rock/paper/scissors: "]) |> String.trim() |> String.to_atom()
 
     cond do
-      ai_choice == player_choice -> IO.puts("Draw")
-      beats?(ai_choice, player_choice) -> IO.puts("AI WIN")
-      beats?(player_choice, ai_choice) -> IO.puts("You WIN")
-      true -> raise(" your input is invalid. Please write in a correct way: rock/paper/scissors")
-    end
+      ai_choice == player_choice ->
+        IO.puts("Draw")
 
+      beats?(ai_choice, player_choice) ->
+        IO.puts("AI WIN")
+
+      beats?(player_choice, ai_choice) ->
+        IO.puts("You WIN")
+
+      true ->
+        raise("your input is invalid. Please correct this: rock/paper/scissors")
+    end
     if player_choice == ai_choice do
       play()
     end
+
   end
 
   defp beats?(player1, player2) do
